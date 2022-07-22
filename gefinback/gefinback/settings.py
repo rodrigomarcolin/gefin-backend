68607117272401195fc2f273f04b43a0056900b3
rodrigomarcolin <rodrigosalesmarcolin@gmail.com>
2022-07-22 02:55:58 +0000

    allow all hosts

--- gefinback/gefinback/settings.py
+++ gefinback/gefinback/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s&3v5&zi50&j_3)%dxk*d=0i5e#81=ukb0p)x-h0^pqdu%6)%v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
