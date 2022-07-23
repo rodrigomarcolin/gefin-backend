5492212bdef6b38c7cbeb0561737dd99a22dcf59
rodrigosm29 <rodrigosalesmarcolin@gmail.com>
2022-07-23 00:03:44 -0300

    fix model bugs

--- gefinback/corelogic/models.py
+++ gefinback/corelogic/models.py
@@ -13,14 +13,14 @@ class ContaBancariaModel(models.Model):
     nome = models.CharField(max_length=200)
     desc = models.CharField(max_length=512)
     dono = models.ForeignKey(User, on_delete=models.CASCADE)
-    banco = models.ForeignKey(BancoModel, on_delete=models.SET_NULL, blank=True)
+    banco = models.ForeignKey(BancoModel, on_delete=models.SET_NULL, blank=True, null=True)
 
 class ControleModel(models.Model):
     nome = models.CharField(max_length=200)
     desc = models.CharField(max_length=512)
     quantia = models.FloatField(default = 0)
     conta = models.ForeignKey(ContaBancariaModel, on_delete=models.CASCADE)
-    data = models.DateField(auto_now=True, auto_now_add=True)
+    data = models.DateField(auto_now_add=True)
     gasto = models.FloatField(default=0)
     tipoGasto = models.BooleanField()
     recorrente = models.BooleanField()
@@ -28,14 +28,14 @@ class ControleModel(models.Model):
 class TransacaoModel(models.Model):
     quantia = models.FloatField(default=0)
     nome = models.CharField(max_length=200)
-    data = models.DateField(auto_now=True, auto_now_add=True)
-    categoria = models.ForeignKey(CategoriaModel, on_delete=models.SET_NULL, blank=True)
-    controle = models.ForeignKey(ControleModel, on_delete=models.SET_NULL, blank=True)
+    data = models.DateField(auto_now_add=True)
+    categoria = models.ForeignKey(CategoriaModel, on_delete=models.SET_NULL, blank=True, null=True)
+    controle = models.ForeignKey(ControleModel, on_delete=models.SET_NULL, blank=True, null=True)
     conta = models.ForeignKey(ContaBancariaModel, on_delete=models.CASCADE)
 
 class TransacaoRecorrenteModel(models.Model):
     quantia = models.FloatField(default=0)
     nome = models.CharField(max_length=200)
-    categoria = models.ForeignKey(CategoriaModel, on_delete=models.SET_NULL)
+    categoria = models.ForeignKey(CategoriaModel, on_delete=models.SET_NULL, null=True)
     conta = models.ForeignKey(ContaBancariaModel, on_delete=models.CASCADE)
     pago_no_mes = models.BooleanField()
