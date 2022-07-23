5036a8fffdb21a2eb329dfdcf8624061af9f596b
rodrigosm29 <rodrigosalesmarcolin@gmail.com>
2022-07-22 23:55:31 -0300

    update models and create request documentation

--- gefinback/corelogic/models.py
+++ gefinback/corelogic/models.py
@@ -1,18 +1,6 @@
-from importlib.metadata import requires
-from pyexpat import model
-from turtle import ondrag
-from typing_extensions import Required
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
-class ContaBancariaModel(models.Model):
-    quantia = models.FloatField(default=0)
-    nome = models.CharField(max_length=200)
-    desc = models.CharField(max_length=512)
-    dono = models.ForeignKey
-    banco = models.ForeignKey(BancoModel, on_delete=models.SET_NULL, blank=True)
-
 
 class BancoModel(models.Model):
     nome = models.CharField(max_length=200)
@@ -20,13 +8,22 @@ class BancoModel(models.Model):
 class CategoriaModel(models.Model):
     nome = models.CharField(max_length=200)
 
+class ContaBancariaModel(models.Model):
+    quantia = models.FloatField(default=0)
+    nome = models.CharField(max_length=200)
+    desc = models.CharField(max_length=512)
+    dono = models.ForeignKey(User, on_delete=models.CASCADE)
+    banco = models.ForeignKey(BancoModel, on_delete=models.SET_NULL, blank=True)
+
 class ControleModel(models.Model):
     nome = models.CharField(max_length=200)
     desc = models.CharField(max_length=512)
     quantia = models.FloatField(default = 0)
     conta = models.ForeignKey(ContaBancariaModel, on_delete=models.CASCADE)
-    mes = models.DateField(auto_now=True, auto_now_add=True)
+    data = models.DateField(auto_now=True, auto_now_add=True)
+    gasto = models.FloatField(default=0)
     tipoGasto = models.BooleanField()
+    recorrente = models.BooleanField()
 
 class TransacaoModel(models.Model):
     quantia = models.FloatField(default=0)
@@ -36,9 +33,9 @@ class TransacaoModel(models.Model):
     controle = models.ForeignKey(ControleModel, on_delete=models.SET_NULL, blank=True)
     conta = models.ForeignKey(ContaBancariaModel, on_delete=models.CASCADE)
 
-class TransacaoRecorrente(models.Model):
+class TransacaoRecorrenteModel(models.Model):
     quantia = models.FloatField(default=0)
     nome = models.CharField(max_length=200)
     categoria = models.ForeignKey(CategoriaModel, on_delete=models.SET_NULL)
     conta = models.ForeignKey(ContaBancariaModel, on_delete=models.CASCADE)
-
+    pago_no_mes = models.BooleanField()
---
+++ gefinback/models.md
@@ -0,0 +1,40 @@
+# PERGUNTAS:
+* Como preencher o id_conta automaticamente?
+
+# Operações para cada um dos modelos e suas consequências
+
+## BancoModel
+* Apenas os desenvolvedores devem poder modificar essa tabela!
+* Automatizar a população da tabela baseada em um índice de bancos? Talvez...
+* Não implementaremos CRUD para este modelo!
+
+## CategoriaModel
+* Apenas os desenvolvedores devem poder modificar essa tabela!
+* Não implementaremos CRUD para este modelo (por enquanto, talvez no futuro cada usuário poderá criar suas próprias categorias)!
+
+## TransacaoRecorrenteModel
+* CRUD simples com Owners
+* A cada mês, seta pago_no_mes = False (o frontend usará esse campo para gerar botões (ou não) que permitirá ao usuário consumar esse gasto)
+* Cada usuário poderá fazer seu próprio CRUD de transações recorrentes e verificar suas próprias transações financeiras
+* Endpoint para consumar TransacaoRecorrente
+* Retrieve não pagos no mês
+
+## ContaBancariaModel
+* CRUD simples com Owners
+
+## ControleModel
+* CRUD simples com Owners
+* Retrieve "normal" verifica apenas Controles do mês atual
+* A cada novo mês, cria novos ControleModels do último mês e que sejam recorrentes para cada usuário! Como isso seria feito? (Airflow? DAG mensal?)
+* Retrieve "historico" retorna os Controles a cada mês de um usuário! (fazer _controller_ com Range!)
+
+## Transacao
+* Create: modifica a ContaBancaria associada de tal forma que ContaBancaria.quantia += Transacao.quantia
+* Delete: modifica a Conta associada de tal forma que ContaBancaria.quantia -= Transacao.quantia
+* Update: faz as seguintes operações: 
+    1. Conta.quantia += Transacao.quantiaNova - Transacao.quantiaVelha
+    1. VelhoControle.gasto -= Transacao.quantiaVelha
+    1. Seleciona NovoControle baseado em Transacao.mesNovo e Transacao.id_controle
+    1. NovoControle.gasto += Transacao.quantiaNova
+* Retrieve "normal": somente no mês atual
+* Retrieve "historico": todos os meses
